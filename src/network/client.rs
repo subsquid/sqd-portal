subsquid/sqd-portal

commit a539d305ffe5ff711a782d4d32cf5e0e35a3ca89

Cleanup
- Remove worker heartbeats support
- Rename gateway to portal everywhere
- Remove stale Helm chart

Cargo.toml
@@ -43,10 +43,10 @@ uuid = { version = "1", features = ["v4", "fast-rng"] }
 [target.'cfg(not(target_env = "msvc"))'.dependencies]
 tikv-jemallocator = { version = "0.6", features = ["profiling"] }
 
-sqd-assignments = { git = "https://github.com/subsquid/sqd-network.git", rev = "b6514fc", features = ["reader"] }
-sqd-contract-client = { git = "https://github.com/subsquid/sqd-network.git", rev = "b6514fc", version = "1.2.1" }
-sqd-messages = { git = "https://github.com/subsquid/sqd-network.git", rev = "b6514fc", version = "2.0.1", features = ["semver", "bitstring", "assignment_reader"] }
-sqd-network-transport = { git = "https://github.com/subsquid/sqd-network.git", rev = "b6514fc", version = "3.0.0", features = ["gateway", "metrics"] }
+sqd-assignments = { git = "https://github.com/subsquid/sqd-network.git", rev = "138b951", features = ["reader"] }
+sqd-contract-client = { git = "https://github.com/subsquid/sqd-network.git", rev = "138b951", version = "1.2.1" }
+sqd-messages = { git = "https://github.com/subsquid/sqd-network.git", rev = "138b951", version = "2.0.1", features = ["semver", "bitstring", "assignment_reader"] }
+sqd-network-transport = { git = "https://github.com/subsquid/sqd-network.git", rev = "138b951", version = "3.0.0", features = ["portal", "metrics"] }
 
 sqd-data-client = { git = "https://github.com/subsquid/data.git", rev = "f68d7d4" }
 sqd-hotblocks = { git = "https://github.com/subsquid/data.git", package = "sqd-node", rev = "f68d7d4" }

src/network/client.rs
@@ -16,7 +16,7 @@ use tokio_util::sync::CancellationToken;
 use sqd_contract_client::{Client as ContractClient, ClientError, PeerId, Worker};
 use sqd_messages::{query_error, query_result, Query, QueryFinished, QueryOk};
 use sqd_network_transport::{
-    get_agent_info, AgentInfo, GatewayConfig, GatewayTransportHandle, Keypair, P2PTransportBuilder,
+    get_agent_info, AgentInfo, Keypair, P2PTransportBuilder, PortalConfig, PortalTransportHandle,
     QueryFailure, TransportArgs,
 };
 
@@ -66,7 +66,7 @@ pub struct NetworkClientStatus {
 
 /// Tracks the network state and handles p2p communication
 pub struct NetworkClient {
-    transport_handle: GatewayTransportHandle,
+    transport_handle: PortalTransportHandle,
     network_state: NetworkState,
     datasets: Arc<RwLock<Datasets>>,
     contract_client: Box<dyn ContractClient>,
@@ -92,13 +92,16 @@ impl NetworkClient {
         let local_peer_id = transport_builder.local_peer_id();
         let keypair = transport_builder.keypair();
 
-        let mut gateway_config = GatewayConfig::default();
-        gateway_config.query_config.request_timeout = config.transport_timeout;
-        gateway_config.query_config.max_concurrent_streams = None;
-        gateway_config.worker_status_via_gossipsub = false;
-        gateway_config.worker_status_via_polling = false;
-        gateway_config.log_sending_timeout = LOGS_SENDING_TIMEOUT;
-        let (_events, transport_handle) = transport_builder.build_gateway(gateway_config)?;
+        let portal_config = PortalConfig {
+            query_config: sqd_network_transport::ClientConfig {
+                max_concurrent_streams: None,
+                request_timeout: config.transport_timeout,
+                ..Default::default()
+            },
+            log_sending_timeout: LOGS_SENDING_TIMEOUT,
+            ..Default::default()
+        };
+        let transport_handle = transport_builder.build_portal(portal_config)?;
 
         let (logs_tx, logs_rx) = if config.send_logs {
             let (tx, rx) = sqd_network_transport::util::new_queue(LOGS_QUEUE_SIZE, "query_logs");

.gitignore
@@ -1,3 +1,4 @@
 /target
 .env
-gateway.key
+*.key
+db

chart/Chart.yaml
@@ -1,5 +1,0 @@
-apiVersion: v2
-appVersion: 0.0.1
-name: subsquid-gateway
-type: application
-version: 0.0.1

chart/templates/cm.yaml
@@ -1,7 +1,0 @@
-apiVersion: v1
-kind: ConfigMap
-metadata:
-  name: gateway-config
-data:
-  gateway_config.yaml: |-
-    {{- include "gateway_config.yaml" . | indent 4 }}

chart/templates/config.tpl
@@ -1,13 +1,0 @@
-{{- define "gateway_config.yaml" }}
-logs_collector_id: {{ .Values.network.logs_collector_peer_id | quote }}
-send_metrics: true
-worker_inactive_threshold_sec: 60
-worker_greylist_time_sec: 120
-default_query_timeout_sec: 60
-summary_print_interval_sec: 0
-workers_update_interval_sec: 60
-available_datasets:
-{{- range $name, $dataset := .Values.datasets }}
-    {{ $name }}: {{ $dataset }}
-{{- end }}
-{{- end }}

chart/templates/gateway.yaml
@@ -1,110 +1,0 @@
-{{ $keys := .Values.gateway.libp2p_private_keys }}
-{{ $gatewaysCount := len $keys }}
----
-apiVersion: apps/v1
-kind: StatefulSet
-metadata:
-  name: gateway
-spec:
-  serviceName: gateway
-  selector:
-    matchLabels:
-      app: gateway
-  replicas: {{ required "Specify at least one private key for gateways" $gatewaysCount }}
-  template:
-    metadata:
-      labels:
-        app: gateway
-      annotations:
-        checksum/secret: {{ include (print $.Template.BasePath "/secrets.yaml") . | sha256sum }}
-        checksum/config: {{ include "gateway_config.yaml" . | sha256sum }}
-    spec:
-      terminationGracePeriodSeconds: 5
-      # podManagementPolicy: Parallel
-      {{- if .Values.gateway.tolerations }}
-      tolerations:
-        {{- toYaml .Values.gateway.tolerations | nindent 8  }}
-      {{- end }}
-      {{- if .Values.gateway.affinity }}
-      affinity:
-        {{- toYaml .Values.gateway.affinity | nindent 8 }}
-      {{- end }}
-      containers:
-      - name: gateway
-        image: {{ .Values.gateway.image }}
-        imagePullPolicy: "Always"
-        ports:
-        - name: http
-          containerPort: 8080
-        env:
-          - name: HTTP_LISTEN_ADDR
-            value: 0.0.0.0:8080
-          - name: BOOT_NODES
-            value: {{ .Values.network.boot_nodes }}
-          - name: POD_NAME
-            valueFrom:
-              fieldRef:
-                fieldPath: metadata.name
-          - name: KEY_PATH
-            value: /run/keys/$(POD_NAME).key
-          - name: CONFIG_PATH
-            value: /run/config/gateway_config.yaml
-          - name: ALLOCATIONS_DB_PATH
-            value: /data/allocations.db
-          {{- if .Values.gateway.logLevel }}
-          - name: RUST_LOG
-            value: {{ .Values.gateway.logLevel }}
-          {{- end }}
-          - name: RPC_URL
-            value: {{ .Values.rpc.url }}
-          - name: L1_RPC_URL
-            value: {{ .Values.rpc.l1_url }}
-          - name: NETWORK
-            value: {{ .Values.rpc.network }}
-          - name: MTU_DISCOVERY_MAX
-            value: "{{ .Values.network.mtu_discovery_max }}"
-        volumeMounts:
-          - mountPath: /run/keys
-            name: keys
-            readOnly: true
-          - mountPath: /run/config
-            name: conf
-            readOnly: true
-          - mountPath: /data
-            name: epoch-data
-        livenessProbe:
-          tcpSocket:
-            port: http
-          initialDelaySeconds: 1
-          periodSeconds: 30
-        resources:
-          requests:
-            cpu: {{ .Values.gateway.resources.requests.cpu | quote }}
-            memory: {{ .Values.gateway.resources.requests.memory | quote }}
-      volumes:
-        - name: keys
-          secret:
-            secretName: secrets
-            items:
-              {{- range $index, $_ := $keys }}
-              - key: gateway-{{ $index }}.key
-                path: gateway-{{ $index }}.key
-              {{- end }}
-        - name: conf
-          configMap:
-            name: gateway-config
-            items:
-              - key: gateway_config.yaml
-                path: gateway_config.yaml
-        - name: epoch-data
-          persistentVolumeClaim:
-            claimName: epoch-data
-  volumeClaimTemplates:
-    - metadata:
-        name: epoch-data
-      spec:
-        accessModes:
-          - ReadWriteOnce
-        resources:
-          requests:
-            storage: 1G

chart/templates/ingress.yaml
@@ -1,22 +1,0 @@
-{{- if .Values.ingress.host  }}
----
-apiVersion: networking.k8s.io/v1
-kind: Ingress
-metadata:
-  name: gateway
-  annotations:
-    kubernetes.io/ingress.class: {{ .Values.ingress.class }}
-    nginx.ingress.kubernetes.io/service-upstream: "true"
-spec:
-  rules:
-    - host: {{ .Values.ingress.host }}
-      http:
-        paths:
-          - path: /
-            pathType: Prefix
-            backend:
-              service:
-                name: gateway
-                port:
-                  number: 8080
-{{- end }}

commit d3884d0985940b80a4e4aea894e28db6ae0e0652

Fix response size limit
The bug was introduced in a539d30

Cargo.toml
@@ -1,6 +1,6 @@
 [package]
 name = "sqd-portal"
-version = "0.7.11"
+version = "0.7.12"
 edition = "2021"
 
 [dependencies]

src/network/client.rs
@@ -94,15 +94,12 @@ impl NetworkClient {
         let local_peer_id = transport_builder.local_peer_id();
         let keypair = transport_builder.keypair();
 
-        let portal_config = PortalConfig {
-            query_config: sqd_network_transport::ClientConfig {
-                max_concurrent_streams: None,
-                request_timeout: config.transport_timeout,
-                ..Default::default()
-            },
+        let mut portal_config = PortalConfig {
             log_sending_timeout: LOGS_SENDING_TIMEOUT,
             ..Default::default()
         };
+        portal_config.query_config.max_concurrent_streams = None;
+        portal_config.query_config.request_timeout = config.transport_timeout;
         let transport_handle = transport_builder.build_portal(portal_config)?;
 
         let (logs_tx, logs_rx) = if config.send_logs {